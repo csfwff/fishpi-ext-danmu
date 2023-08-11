ac6d560284264a0f50cf0be26f97ab6273d44467
csfwff <csfwff@163.com>
2023-08-11 09:23:42 +0800

    🐛 修复发送消息重复的问题

--- extension/event.js
+++ extension/event.js
@@ -1,9 +1,11 @@
+let isListening = false
 module.exports = function eventListen({ ipcMain }, getSetting,sendMsg) {
+  if(isListening)return;
+  isListening = true
   ipcMain.on(`danmu.get.setting`, (event, args) => {
     event.sender.send(`danmu.get.setting.` + args.callback, getSetting())
   });
   ipcMain.on(`fishpi.msg.send`,(event, args)=>{
-    console.log(args.msg)
     sendMsg(args.msg)
   })
 }
